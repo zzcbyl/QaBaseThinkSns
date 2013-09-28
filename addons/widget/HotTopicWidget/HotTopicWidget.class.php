53e7c0c2a36da4ef2b23424b62ba865ca11c63b2
zzcbyl <cn36312@126.com>
2013-09-28 16:13:42 +0800

    add new files

---
+++ addons/widget/HotTopicWidget/HotTopicWidget.class.php
@@ -0,0 +1,31 @@
+﻿<?php
+
+class HotTopicWidget extends Widget {
+	
+
+	private $limitnums   = 10;
+
+	public function render($data) {
+		$topic = $this->getHotTopicData();
+		
+		return $topic;
+	}
+	
+	/**
+	获取热门话题
+	*/
+	private function getHotTopicData()
+	{
+		$where =' (is_audit=1 OR is_audit=0) AND is_del = 0 AND feed_questionid=0 ';
+		$list = model('Feed')->getList($where,$this->limitnums,'answer_count desc, feed_id desc');
+		$var['data'] = $list['data'];
+
+		//渲染模版
+		$content = $this->renderFile(dirname(__FILE__)."/HotTopic.html",$var);
+		
+		return $content;
+	}
+	
+}
+
+?>
