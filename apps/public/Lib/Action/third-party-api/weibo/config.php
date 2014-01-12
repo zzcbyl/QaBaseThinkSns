61252fd4e7ae081bf1de96d9e9d023c62f5cd49c
cangjie <cangjie@hotmail.com>
2014-01-12 17:52:03 +0800

    weibo login callback

--- apps/public/Lib/Action/third-party-api/weibo/config.php
+++ apps/public/Lib/Action/third-party-api/weibo/config.php
@@ -3,4 +3,4 @@ header('Content-Type: text/html; charset=UTF-8');
 
 define( "WB_AKEY" , '1252918289' );
 define( "WB_SKEY" , '5e7e892805047d65108e3c866d330ff4' );
-define( "WB_CALLBACK_URL" , 'http://www.luqinwenda.cn/weibologin/callback.php' );
+define( "WB_CALLBACK_URL" , 'http://www.luqinwenda.cn/index.php?app=public&mod=Register&act=weibologincallback' );
