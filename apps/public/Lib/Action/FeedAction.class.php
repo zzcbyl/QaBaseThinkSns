1b47fa287008a902232df3206f6b6c14dcf3b39e
zzcbyl <cn36312@126.com>
2014-03-17 11:55:28 +0800

    新浪微博分享

--- apps/public/Lib/Action/FeedAction.class.php
+++ apps/public/Lib/Action/FeedAction.class.php
@@ -115,25 +115,7 @@ class FeedAction extends Action {
 		//分享到微博
 		if($_POST['ShareSina'] == "1")
 		{
-			$loginData = model('Login')->get($data['uid']);
-			if($loginData['oauth_token'] != '')
-			{
-				$contentTxt = urlencode(substr($data['description'], 0, 272).'...　请 @卢勤问答网站 来帮帮我。'.SITE_URL.'/index.php%3Fapp=public%26mod=Passport%26act=newquestion%26feed_id='.$data['feed_id']);
-			
-				$urlPar = 'http://sync.luqinwenda.cn/sync.aspx?oriid='.$data['feed_id'].'&token='.$loginData['oauth_token'].'&content='.$contentTxt;
-				// 初始化一个 cURL 对象
-				$curl = curl_init();
-				// 设置你需要抓取的URL
-				curl_setopt($curl, CURLOPT_URL, $urlPar);
-				// 设置header
-				curl_setopt($curl, CURLOPT_HEADER, 1);
-				// 设置cURL 参数，要求结果保存到字符串中还是输出到屏幕上。
-				curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-				// 运行cURL，请求网页
-				$dhtml = curl_exec($curl);
-				// 关闭URL请求
-				curl_close($curl);
-			}
+			$this->shareWeiBo($data['uid'], $data['feed_id'], $data['description']);
 		}
 		
 		// 发布邮件之后添加积分
@@ -207,6 +189,68 @@ class FeedAction extends Action {
 
 		exit(json_encode($return));
 	}
+	
+	/**
+	 * 分享到新浪微博(js)
+	 *
+	 * @return mixed This is the return value description
+	 *
+	 */
+	public function shareSina()
+	{
+		$feedid = $_POST['feed_id'];
+		if(empty($feedid)||$feedid<=0)
+		{
+			$return = array('status'=>0,'data'=>'分享失败');
+			exit(json_encode($return));
+		}
+		$feedData = model('feed')->getFeeds(array($feedid));
+		if(!empty($feedData))
+		{
+			$this->shareWeiBo($this->mid, $feedData[0]['feed_id'], $feedData[0]['description']);
+			$return = array('status'=>1,'data'=>'分享成功');
+		}
+		exit(json_encode($return));
+	}
+	
+	//分享到新浪微博
+	public function shareWeiBo($uid, $feed_id, $description)
+	{
+		$loginData = model('Login')->get($uid);
+		if($loginData['oauth_token'] != '')
+		{
+			$contentTxt = urlencode(utf_substr($description, 180).'...　请 @卢勤问答网站 @知心姐姐卢勤 来帮帮我。http://www.luqinwenda.com/index.php%3Fapp=public%26mod=Passport%26act=newquestion%26feed_id='.$feed_id);
+			
+			$urlPar = 'http://sync.luqinwenda.cn/sync.aspx?oriid='.$feed_id.'&token='.$loginData['oauth_token'].'&content='.$contentTxt;
+			// 初始化一个 cURL 对象
+			$curl = curl_init();
+			// 设置你需要抓取的URL
+			curl_setopt($curl, CURLOPT_URL, $urlPar);
+			// 设置header
+			curl_setopt($curl, CURLOPT_HEADER, 1);
+			// 设置cURL 参数，要求结果保存到字符串中还是输出到屏幕上。
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+			// 运行cURL，请求网页
+			$dhtml = curl_exec($curl);
+			// 关闭URL请求
+			curl_close($curl);
+		}
+	}
+	
+	public function chinesesubstr($str, $start, $len) { // $str指字符串,$start指字符串的起始位置，$len指字符串长度
+		$strlen = $start + $len; // 用$strlen存储字符串的总长度，即从字符串的起始位置到字符串的总长度
+		for($i = $start; $i < $strlen;) {
+			if (ord ( substr ( $str, $i, 1 ) ) > 0xa0) { // 如果字符串中首个字节的ASCII序数值大于0xa0,则表示汉字
+				$tmpstr .= substr ( $str, $i, 3 ); // 每次取出三位字符赋给变量$tmpstr，即等于一个汉字
+				$i=$i+3; // 变量自加3
+			} else{
+				$tmpstr .= substr ( $str, $i, 1 ); // 如果不是汉字，则每次取出一位字符赋给变量$tmpstr
+				$i++;
+			}
+		}
+		return $tmpstr; // 返回字符串
+	}
+	
 	
 	/**
 	 * 分享/转发提问操作，需要传入POST的值
