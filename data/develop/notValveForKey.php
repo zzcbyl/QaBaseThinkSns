55e7a9e1b6f6e7d987fb683a9eb361580fdfa031
zzcbyl <cn36312@126.com>
2014-06-12 17:25:04 +0800

    data文件

--- data/develop/notValveForKey.php
+++ data/develop/notValveForKey.php
@@ -95,5 +95,6 @@ return array (
   '<BR />已经邀请过“CN36307”<BR />已经邀请过“CN36302”<BR />' => '?app=public&mod=profile&act=invitefriendanswer',
   '<BR />邀请“CN36301”成功<BR />邀请“底蕴”成功<BR />' => '?app=public&mod=profile&act=invitefriendanswer',
   'PUBLIC_CANCEL' => '?app=public&mod=Register&act=avatar',
+  '<BR />邀请“管理员”成功<BR />' => '?app=public&mod=profile&act=invitefriendanswer',
 );
 ?>
