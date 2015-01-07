08438fcee10a5545856fd55b0a1ec3138a42c91f
zzcbyl <cn36312@126.com>
2015-01-07 09:36:57 +0800

    update

--- index.php
+++ index.php
@@ -1,20 +1,12 @@
 <?php
 error_reporting(E_ERROR);
 define('APP_DEBUG',TRUE);
+
 /** ///调试、找错时请去掉///前空格
 ini_set('display_errors',true);
 error_reporting(E_ALL); 
 set_time_limit(0);
 **/
-
-//安装检查开始：如果您已安装过卢勤问答，可以删除本段代码
-if(is_dir('install') && !file_exists('install/install.lock')){
-	header("Content-type: text/html; charset=utf-8");
-	die ("<div style='border:2px solid green; background:#f1f1f1; padding:20px;margin:20px;width:800px;font-weight:bold;color:green;text-align:center;'>"
-		."<h1>您尚未安装卢勤问答系统，<a href='install/install.php'>请点击进入安装页面</a></h1>"
-		."</div> <br /><br />");
-}
-//安装检查结束
 
 //网站根路径设置
 define('SITE_PATH', dirname(__FILE__));
